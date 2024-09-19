7405d6e9bbffee542a31b55294fb1516dbd27c91
Mike Battye <mbattye@me.com>
2024-09-19 15:05:27 +0100

    new page layout

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 import streamlit as st
-from pages.dashboard import dashboard_page
+from pages.home import home_page
 from pages.health_analysis import health_analysis_page
+from pages.settings import settings_page
 
 def main():
     st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to", ["Dashboard", "Health Analysis"])
+    page = st.sidebar.radio("Go to", ["Home", "Health Analysis", "Settings"])
 
-    if page == "Dashboard":
-        dashboard_page()
+    if page == "Home":
+        home_page()
     elif page == "Health Analysis":
         health_analysis_page()
+    elif page == "Settings":
+        settings_page()
 
 if __name__ == "__main__":
     main()
