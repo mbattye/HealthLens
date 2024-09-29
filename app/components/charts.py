03f771f35ed60f774a162620b8895a09a5bf5466
Mike Battye <mbattye@me.com>
2024-09-29 18:29:41 +0100

    updates to data tables and charts

--- app/components/charts.py
+++ app/components/charts.py
@@ -2,16 +2,31 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
+custom_colors = {
+    'Run': '#ef476f',
+    'Ride': '#ffd166',
+    'Swim': '#06d6a0',
+    'Walk': '#118ab2',
+    'Hike': '#073b4c',
+    'Workout': '#84a59d',
+    'WeightTraining': '#f28482',
+    'Yoga': '#f5cac3',
+    'RockClimbing': '#84a59d',
+    'Kayaking': '#f6bd60',
+    # Add more activity types and colors as needed
+}
+
 def plot_activity_types(df):
-    if 'Type' not in df.columns:
+    if 'type' not in df.columns:
         st.warning("Activity type data is not available.")
         return
     
-    activity_counts = df['Type'].value_counts()
+    activity_counts = df['type'].value_counts()
     top_activities = activity_counts.head(4)
     other_activities = activity_counts[4:].sum()
-    activity_counts = top_activities.append(pd.Series({'Other': other_activities}))
-    
+    # activity_counts = top_activities.append(pd.Series({'Other': other_activities}))
+    activity_counts = pd.concat([top_activities, pd.Series({'Other': other_activities})])
+
     colors = ['#ef476f', '#ffd166', '#06d6a0', '#118ab2', '#073b4c']
     fig = px.pie(values=activity_counts.values, names=activity_counts.index, title='Activity Types', color_discrete_sequence=colors)
     st.plotly_chart(fig)
@@ -21,35 +36,57 @@ def plot_distance_over_time(df):
     st.plotly_chart(fig)
 
 def plot_activity_heatmap(df):
-    if 'Start Date' not in df.columns:
+    if 'start_date' not in df.columns:
         st.warning("Activity start date data is not available.")
         return
     
-    df['Start Date'] = pd.to_datetime(df['Start Date'])
-    df['Weekday'] = df['Start Date'].dt.day_name()
-    df['Hour'] = df['Start Date'].dt.hour
-    heatmap_data = df.groupby(['Weekday', 'Hour']).size().reset_index(name='Count')
-    fig = px.density_heatmap(heatmap_data, x='Hour', y='Weekday', z='Count', title='Activity Heatmap', nbinsx=24)
+    df['start_date'] = pd.to_datetime(df['start_date'])
+    df['weekday'] = df['start_date'].dt.day_name()
+    df['hour'] = df['start_date'].dt.hour
+    heatmap_data = df.groupby(['weekday', 'hour']).size().reset_index(name='Count')
+    fig = px.density_heatmap(heatmap_data, x='hour', y='weekday', z='Count',\
+                             title='Activity Heatmap',\
+                             nbinsx=24)
+    fig.update_layout(
+    xaxis_title='Day',
+    yaxis_title='Month'
+    )
+    fig.update_coloraxes(colorbar_title_text='Count')
+    
     st.plotly_chart(fig)
 
 def plot_activity_duration(df):
-    if 'Moving Time' not in df.columns or 'Start Date' not in df.columns:
+    if 'moving_time' not in df.columns or 'start_date' not in df.columns:
         st.warning("Activity duration data is not available.")
         return
     
-    df['Start Date'] = pd.to_datetime(df['Start Date'])
-    df['Duration (hours)'] = df['Moving Time'] / 3600
-    fig = px.scatter(df, x='Start Date', y='Duration (hours)', color='Type', title='Activity Duration Over Time', height=600)
+    df['start_date'] = pd.to_datetime(df['start_date'])
+    df['duration_hours'] = df['moving_time'] / 3600
+    fig = px.scatter(df, x='start_date', y='duration_hours', color='type', title='Activity Duration Over Time', height=600, color_discrete_map=custom_colors)
+    
+    fig.update_layout(
+    xaxis_title='Date',
+    yaxis_title='Duration (hours)'
+    )
+    fig.update_layout(legend_title_text='Activity Type')
+    
     st.plotly_chart(fig)
 
 def plot_activity_distance(df):
-    if 'Distance' not in df.columns or 'Start Date' not in df.columns:
+    if 'distance' not in df.columns or 'start_date' not in df.columns:
         st.warning("Activity distance data is not available.")
         return
     
-    df['Start Date'] = pd.to_datetime(df['Start Date'])
-    df['Distance (km)'] = df['Distance'] / 1000
-    fig = px.scatter(df, x='Start Date', y='Distance (km)', color='Type', title='Activity Distance Over Time', height=600)
+    df['start_date'] = pd.to_datetime(df['start_date'])
+    df['distance_km'] = df['distance'] / 1000
+    fig = px.scatter(df, x='start_date', y='distance_km', color='type', title='Activity Distance Over Time', height=600, color_discrete_map=custom_colors)
+
+    fig.update_layout(
+    xaxis_title='Date',
+    yaxis_title='Distance (km)'
+    )
+    fig.update_layout(legend_title_text='Activity Type')
+    
     st.plotly_chart(fig)
 
 # Add more chart functions as needed
--- app/components/data_tables.py
+++ app/components/data_tables.py
@@ -3,8 +3,32 @@ import pandas as pd
 
 def display_summary_table(summary):
     summary_df = pd.DataFrame(list(summary.items()), columns=['Metric', 'Value'])
-    st.table(summary_df)
+    summary_df.columns = [x.replace("_", " ").title() for x in summary_df.columns]
+    # Convert 'Value' column to numeric, replacing any non-numeric values with NaN
+    summary_df['Value'] = pd.to_numeric(summary_df['Value'], errors='coerce')
+    
+    # Round the first 3 rows to integers and the 4th row to 1 decimal place
+    summary_df.loc[:2, 'Value'] = summary_df.loc[:2, 'Value'].round(0).astype(int)
+    if len(summary_df) > 3:
+        summary_df.loc[3, 'Value'] = summary_df.loc[3, 'Value'].round(1)
+    
+    # Display the table with custom styling
+    st.dataframe(
+        summary_df,
+        hide_index=True,
+        column_config={
+            "Metric": st.column_config.TextColumn(
+                "Metric",
+                width="medium",
+            ),
+            "Value": st.column_config.NumberColumn(
+                "Value",
+                format="%.1f",
+            ),
+        },
+    )
 
 def display_activities_table(df):
     df = df.drop(columns=['resource_state', 'athlete', 'id', 'start_date_local', 'timezone', 'utc_offset', 'location_city', 'location_state', 'location_country', 'achievement_count', 'kudos_count', 'comment_count', 'athlete_count', 'photo_count', 'map', 'trainer', 'commute', 'manual', 'private', 'visibility', 'flagged', 'gear_id', 'start_latlng', 'end_latlng', 'average_speed', 'max_speed', 'has_heartrate', 'average_heartrate', 'max_heartrate', 'heartrate_opt_out', 'display_hide_heartrate_option', 'elev_high', 'elev_low', 'upload_id', 'upload_id_str', 'external_id', 'from_accepted_tag', 'pr_count', 'total_photo_count', 'has_kudoed', 'suffer_score', 'average_watts', 'kilojoules', 'device_watts', 'average_temp'])
+    df.columns = [x.replace("_", " ").title() for x in df.columns]
     st.dataframe(df)
